5392e35d0517c5489f99b427706cfef60bbe4fd5
Bishop <christianiradukunda6@gmail.com>
2023-04-28 23:29:19 +0200

    using curl to send api requests

--- index.php
+++ index.php
@@ -1,37 +1,19 @@
 <?php
+// using curl to open apis
 
-if(!empty($_GET['name'])){
-$response = file_get_contents("https://api.agify.io?name={$_GET['name']}");
-// converting json into php array
-$data = json_decode($response,true);
 
-$age = $data['age'];
-}
+$curl_handle = curl_init();
+
+// setting the url option to our handle
+
+curl_setopt($curl_handle,CURLOPT_URL,"https://randomuser.me/api");
+curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true); // returning string from the reques url
+
+// writting out the response text to a new variable
+
+$response = curl_exec($curl_handle);
+curl_close($curl_handle);
+
+echo $response,'\n';
+
 ?>
-<?php if(isset($age)):?>
-    Age : <?php echo $age ;?>
-
-    <?php endif;?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>testing API in form</title>
-</head>
-<body>
-    <center>
-    <form action="">
-        <label for="firstname"></label>
-        <input type="text" id="name" name="name" placeholder ="Enter your name">
-        <br>
-        <br>
-        <button>Guess my age</button>
-    </form>
-    </center>
-    
-    
-</body>
-</html>
