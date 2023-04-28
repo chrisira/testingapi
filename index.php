aee8d2dba3d87ab0ec97f4c2399125312342e963
Bishop <christianiradukunda6@gmail.com>
2023-04-29 00:05:27 +0200

    using array to handle multiple options for the request

--- index.php
+++ index.php
@@ -6,9 +6,19 @@ $curl_handle = curl_init();
 
 // setting the url option to our handle
 
+/*
 curl_setopt($curl_handle,CURLOPT_URL,"https://randomuser.me/api");
 curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,true); // returning string from the reques url
+*/
 
+// using curl_setopt_array to handle more that one option
+
+curl_setopt_array($curl_handle,[
+
+    CURLOPT_URL => "https://randomuser.me/api",
+    CURLOPT_RETURNTRANSFER => true]
+
+);
 // writting out the response text to a new variable
 
 $response = curl_exec($curl_handle);
