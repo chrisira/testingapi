bf3df9292b12c774dc6bc37bba2b91def50cf705
Bishop <christianiradukunda6@gmail.com>
2023-04-28 22:35:29 +0200

    converted the json to php array and then extracting data from the array

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 $response = file_get_contents("https://randomuser.me/api");
 
-echo $response;
+
+// converting json into php array
+$data = json_decode($response,true);
+
+// accessing the first element from the result
+echo $data['results'][0]['name']['first'],"\n";
 
 ?>
