920e75ed40b93ca87a60704bffb519ec53525172
Bishop <christianiradukunda6@gmail.com>
2023-04-28 22:51:58 +0200

    Testing simple agify api with html form

--- index.php
+++ index.php
@@ -1,12 +1,37 @@
 <?php
 
-$response = file_get_contents("https://randomuser.me/api");
-
-
+if(!empty($_GET['name'])){
+$response = file_get_contents("https://api.agify.io?name={$_GET['name']}");
 // converting json into php array
 $data = json_decode($response,true);
 
-// accessing the first element from the result
-echo $data['results'][0]['name']['first'],"\n";
-
+$age = $data['age'];
+}
 ?>
+<?php if(isset($age)):?>
+    Age : <?php echo $age ;?>
+
+    <?php endif;?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>testing API in form</title>
+</head>
+<body>
+    <center>
+    <form action="">
+        <label for="firstname"></label>
+        <input type="text" id="name" name="name" placeholder ="Enter your name">
+        <br>
+        <br>
+        <button>Guess my age</button>
+    </form>
+    </center>
+    
+    
+</body>
+</html>
