70efb6497024f9e03bf8112b37d9dfcea868e5df
Bishop <christianiradukunda6@gmail.com>
2023-05-14 16:12:31 +0200

    added structure for guzzle api manager

---
+++ guzzle.php
@@ -0,0 +1,21 @@
+<?php
+require __DIR__ ."/vendor/autoload.php";
+
+// creating new guzzle client object
+$client = new GuzzleHttp\client;
+
+// sending the request to the api
+
+$response = $client->request("GET","https://api.github.com/user/repos",[
+    "headers"=>[
+        "Authorization" =>"token YOUR_TOKEN",
+        "User-Agent"=>"chrisira"
+    ]
+    ]);
+// printing out the responses
+echo $response->getStatusCode(),"\n";
+echo $response->getHeader("Content-type")[0],"\n";
+echo substr($response->getBody(),0,200),"...\n";
+
+
+?>
