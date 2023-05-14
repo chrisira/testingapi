b1d51b2545ee7438744de800bf286d98b5389d71
Bishop <christianiradukunda6@gmail.com>
2023-05-14 15:36:09 +0200

    getting the details of one specific gist using id

--- gists.php
+++ gists.php
@@ -5,7 +5,7 @@
 $curl_handle = curl_init();
 curl_setopt_array($curl_handle,[
 
-    CURLOPT_URL => "https://api.github.com/gists",
+    CURLOPT_URL => "https://api.github.com/gists/608e5576f3e559731a9ad37c12fd2483",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_USERAGENT=> "chrisira",
 
@@ -15,6 +15,9 @@ curl_setopt_array($curl_handle,[
 $response = curl_exec($curl_handle);
 curl_close($curl_handle);
 
+$data = json_decode($response,true);
+
+print_r($data);
 
 
 ?>
